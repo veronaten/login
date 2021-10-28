5b30b691c2105cfbb4567e1ae730223c33b42bcd
Veronika <verona.binko@gmail.com>
2021-10-28 11:08:58 +0300

    refactor: add Input Componentand change file login.js

---
+++ src/store/auth-context.js
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react';
+
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {}
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    return setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1');
+    return setIsLoggedIn(true);
+  };
+
+  return <AuthContext.Provider 
+    value={{
+      isLoggedIn: isLoggedIn,
+      onLogout: logoutHandler,
+      onLogin: loginHandler
+    }}
+  >
+    {props.children}
+  </AuthContext.Provider>
+}
+
+export default AuthContext;
